c4e34edb1ab01feb2d4c84a0d63359edfc40c294
marlowe-elmiger <marlowe.elmiger@gmail.com>
2025-11-03 19:26:11 +0000

    tested anomaly_model.py and updated git.ignore file

--- anomaly_model.py
+++ anomaly_model.py
@@ -38,8 +38,8 @@ class AnomalyModel:
          if self.model is None:
             raise ValueError("Model not trained yet.")
         
-        #  will return 1 for normal and -1 for anomaly
-        return self.model.predict(features)
+         #  will return 1 for normal and -1 for anomaly
+         return self.model.predict(features)
     
     # This function saves the trained model to a file
     def save(self, filepath):
@@ -62,5 +62,50 @@ class AnomalyModel:
 
 # testing
 if __name__ == "__main__":
-    detector = AnomalyModel()
-    print("done")
+    from log_gatherer import LogGatherer
+    from log_parser import LogParser
+    from feature_extractor import FeatureExtractor
+    
+
+    
+    
+    print("Preparing training data...")
+    gatherer = LogGatherer()
+    parser = LogParser()
+    extractor = FeatureExtractor()
+    
+    logs = gatherer.read_logs(max_lines=100000)
+    parsed = parser.parse_all(logs)
+    features = extractor.extract_all(parsed)
+    print(f"Extracted features from {len(features)} logs\n")
+    
+    
+    print("Training model...")
+    detector = AnomalyModel(contamination=0.015)
+    detector.train(features)
+   
+    
+    
+    print("\nRunning predictions...")
+    predictions = detector.predict(features)
+    
+    
+    
+    
+    print("\n 30 Sample anomalies:")
+    shown = 0
+    for pred, log in zip(predictions, parsed):
+        if pred == -1:
+            shown += 1
+            print(f"  {shown}. [{log['process']}] {log['message']}...")
+            if shown >= 30:
+                break
+
+
+
+    detector.save("trained_model.pkl")
+    new_detector = AnomalyModel()
+    new_detector.load("trained_model.pkl")
+    test_pred = new_detector.predict([features[0]])
+    
+    
