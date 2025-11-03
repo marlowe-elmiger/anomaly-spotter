7a8f5444d1acb4ab7e1a7a984ef2166b633778d6
marlowe-elmiger <marlowe.elmiger@gmail.com>
2025-11-03 16:41:15 +0000

    added save function

--- anomaly_model.py
+++ anomaly_model.py
@@ -6,7 +6,8 @@ Date: 11/3/2025
 
 
 """
-
+# used for serialization
+import pickle
 from sklearn.ensemble import IsolationForest
 
 
@@ -39,6 +40,16 @@ class AnomalyModel:
         
         #  will return 1 for normal and -1 for anomaly
         return self.model.predict(features)
+    
+    # This function saves the trained model to a file
+    def save(self, filepath):
+        if self.model is None:
+            raise ValueError("No model")
+        
+        # Opens the file in binary write mode and saves the model using pickle
+        with open(filepath, 'wb') as f:
+            pickle.dump(self.model, f)
+        print(f"Model saved to {filepath}")
 
 
 # testing
