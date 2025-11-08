06efc5c4809587f87756469866427edf7371c4c9
Smogel912 <103071961+Smogel912@users.noreply.github.com>
2025-11-07 22:28:59 -0500

    Updated webhooks URL

--- alert.py
+++ alert.py
@@ -7,7 +7,7 @@ Date: 11/5/2025
 
 import requests
 
-WEBHOOK_URL = "https://discordapp.com/api/webhooks/1421172625173905478/CL8QgyVAdeHtBUAAtKHZ_THzsbtiYeOb8uBvPohvcu4QAmEFFuoEkP0nFBTzrEWV7oVM"
+WEBHOOK_URL = https://discordapp.com/api/webhooks/1421173634461859984/hRHsfdT0XDG6VJues-ihUWPSIEOSy0v4ZdlMPKjmfLZ7XckOO4Zecy-dyQzhyHiSNI5P
 
 def send_alert(message):
     data = {"content": message}
